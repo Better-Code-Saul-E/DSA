e045f117ea8a86f897dfa5301dcedbfe0d73ab42
Saul Enriquez <144638754+Better-Code-Saul-E@users.noreply.github.com>
2025-09-18 13:24:26 -0500

    feat: add Big-O demonstration methods in C#

---
+++ Algorithms/Algorithms.cs
@@ -0,0 +1,58 @@
+
+namespace Algorithms;
+
+static class BigONotation
+{
+    // Constant o(1)
+    /*
+        Regardless of how big the List<int> is it will always 
+        take the same amount of time to return the first element.
+    */
+    public static int GetFirstElement(List<int> integers)
+    {
+        return integers[0];
+    }
+
+    // Linear o(n)
+    /*
+        This method cannot skip values, It will have to sift through every 
+        possible value in the List<int> in order to discover if it contains 
+        the value you are looking for
+    */
+    public static bool ContainsValue(List<int> integers, int value)
+    {
+        foreach (int integer in integers)
+        {
+            if (integer == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Quadratic o(n^2)
+    /*
+    Each element must be compared with every other element to discover 
+    any duplicates. This nested loop grows proportionally to 
+    the number of elements n * n
+    */
+    public static List<int> GetDuplicates(List<int> integers)
+    {
+        List<int> duplicates = new List<int>();
+
+        for (int i = 0; i < integers.Count; i++)
+        {
+            for (int j = i + 1; j < integers.Count; j++)
+            {
+                int dupe = integers[i];
+                if (dupe == integers[j] && !duplicates.Contains(dupe))
+                {
+                    duplicates.Add(dupe);
+                }
+            }
+        }
+        return duplicates;
+    }
+
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithms;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>
+        { 1, 2, 3, 4, 5, 6, 7, 8,
+        9, 1, 2, 3, 8, 11, 23, 56,
+        12, 45, 67, 87, 12, 3, 4,
+        87, 23, 45, 1, 1, 34, 90 };
+
+        // Constant
+        int firstNum = BigONotation.GetFirstElement(numbers);
+        Console.WriteLine("This first element of numbers is: " + firstNum);
+
+        // Linear
+        int value = 90;
+        bool containsValue = BigONotation.ContainsValue(numbers, value);
+        Console.WriteLine($"Does numbers contain the value {value}?: " + containsValue);
+
+        // Quadratic
+        List<int> duplicates = BigONotation.GetDuplicates(numbers);
+        Console.WriteLine("The duplicates in numbers are: " + string.Join(",", duplicates));
+    }
+}
